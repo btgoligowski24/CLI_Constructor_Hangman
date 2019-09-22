2ecca1d22d5bb92db22f8ab80d695017aa5c87ae
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-09-22 17:24:30 -0500

    letter and word constructor done, now working on index file

--- letter.js
+++ letter.js
@@ -1,5 +1,19 @@
-function letter() {
-
+function Letter(letter) {
+    this.charStr = letter,
+        this.hasBeenGuessed = false,
+        this.correctGuess = function (char) {
+            if (char.toLowerCase() === this.charStr.toLowerCase()) {
+                this.hasBeenGuessed = true;
+            }
+        }
 }
 
-module.exports = letter;
+Letter.prototype.toString = function () {
+    if (this.hasBeenGuessed === true) {
+        return this.charStr
+    } else {
+        return "_"
+    }
+}
+
+module.exports = Letter;
--- word.js
+++ word.js
@@ -1,3 +1,25 @@
-let letter = require("./letter");
+let Letter = require("./letter");
 
-module.exports = word;
+function Word(chosenWord) {
+    this.letterArr = (function () {
+            let nestedLetterArr = [];
+            for (letters in chosenWord) {
+                nestedLetterArr.push(new Letter(chosenWord[letters]))
+            }
+            return nestedLetterArr;
+        })(),
+        this.wordStr = function () {
+            let wordString = "";
+            for (letters in this.letterArr) {
+                wordString += this.letterArr[letters].toString();
+            }
+            return wordString
+        },
+        this.letterGuess = function (letter) {
+            for (letters in this.letterArr) {
+                this.letterArr[letters].correctGuess(letter)
+            }
+        }
+}
+
+module.exports = Word;
